8d8860d7b40bf6ec9d249bc1147ca4e38e5aa812
Ahmed Tawfik <ahmedtawfik833@gmail.com>
2024-04-19 23:16:54 +0200

    added a not found path handler

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const tasksRouter = require("./routers/tasksRouter");
+const notFound = require("./middleware/notFound");
 
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/api/v1/tasks", tasksRouter);
-
+app.use(notFound);
 module.exports = app;
